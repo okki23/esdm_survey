ad573fa3d22e9f789da0c3858f13aeda796c26cd
supriyadin-inoy-99-co <supri170845@gmail.com>
2022-09-18 15:05:25 +0700

    create table opsi_pertanyaan and addon

---
+++ database/migrations/2022_09_18_075832_create_addon_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addon', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipe_addon')->index();
+            $table->smallInteger('created_by')->nullable()->default(1);
+            $table->smallInteger('updated_by')->nullable()->default(1);
+            $table->smallInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addon');
+    }
+};
